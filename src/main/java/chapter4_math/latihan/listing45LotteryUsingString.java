4712592e07b47c965f536740b84a9b6c5953abce
Nizar Rahmatullah <nizarr292@gmail.com>
2023-01-05 16:34:06 +0700

    first commit

---
+++ src/main/java/chapter4_math/latihan/listing45LotteryUsingString.java
@@ -0,0 +1,35 @@
+package chapter4_math.latihan;
+
+import java.util.Scanner;
+
+public class listing45LotteryUsingString {
+    public static void main(String[] args) {
+        String lottery = "" + (int)(Math.random() * 10)
+                + (int)(Math.random() * 10);
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your lottery pick (two digits): ");
+        String guess = input.nextLine();
+
+        char lotteryDigit1 = guess.charAt(0);
+        char lotteryDigit2 = guess.charAt(1);
+
+        char guessDigit1 = guess.charAt(0);
+        char guessDigit2 = guess.charAt(1);
+
+        System.out.println("The lottery number is " + lottery);
+
+        if (guess.equals(lottery))
+            System.out.println("Exact macth: you win $10,000");
+        else if (guessDigit2 == lotteryDigit1
+                && guessDigit1 == lotteryDigit2)
+            System.out.println("Match all digits: you win $3,000");
+        else if (guessDigit1 == lotteryDigit1
+                || guessDigit1 == lotteryDigit2
+                || guessDigit2 == lotteryDigit1
+                || guessDigit2 == lotteryDigit2)
+            System.out.println("Match one digit : you win $1,000");
+        else
+            System.out.println("Sorry, no match");
+    }
+}
